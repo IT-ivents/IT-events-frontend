5b27ac96a18b3ae71a49f0e712fadc4d1061c641
greybirbroman <98167513+greybirbroman@users.noreply.github.com>
2023-05-28 17:35:09 +0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,21 +1,21 @@
 import styles from './Footer.module.css';
 
 const Footer = () => {
-	const date = new Date().getFullYear();
-	const footerList = ['О нас', 'Политика конфиденциональности', 'Cookies'];
-	return (
-		<footer className={styles.footer}>
-			<span className={styles.year}>&copy; {date} IT-events</span>
-			<ul className={styles.footerList}>
-				{footerList.map((item, index) => (
-					<li className={styles.footerItem} key={index}>
-						{item}
-					</li>
-				))}
-			</ul>
-			<div></div>
-		</footer>
-	);
+  const date = new Date().getFullYear();
+  const footerList = ['О нас', 'Политика конфиденциональности', 'Cookies'];
+  return (
+    <footer className={styles.footer}>
+      <span className={styles.year}>&copy; {date} IT-events</span>
+      <ul className={styles.footerList}>
+        {footerList.map((item, index) => (
+          <li className={styles.footerItem} key={index}>
+            {item}
+          </li>
+        ))}
+      </ul>
+      <div></div>
+    </footer>
+  );
 };
 
 export default Footer;
