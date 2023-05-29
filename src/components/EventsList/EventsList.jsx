addb9c3295a7cda551d4c109a7860ce63b7dd2c2
greybirbroman <98167513+greybirbroman@users.noreply.github.com>
2023-05-29 09:52:35 +0300

    feat: add PageSelector on Main / EventList logic in Event

--- src/components/EventsList/EventsList.jsx
+++ src/components/EventsList/EventsList.jsx
@@ -1,10 +1,17 @@
+import { useLocation } from 'react-router-dom';
 import styles from './EventsList.module.css';
 import EventCard from '../EventCard/EventCard';
+import ListSelector from '../ListSelector/ListSelector';
 
 const EventsList = ({ title, list, onCardClick }) => {
+  const location = useLocation();
+
   return (
     <section className={styles.section}>
-      <h2 className={styles.title}>{title}</h2>
+      <div className={styles.container}>
+        <h2 className={styles.title}>{title}</h2>
+        {location.pathname === '/' && <ListSelector />}
+      </div>
       <ul className={styles.list}>
         {list.map((event) => (
           <EventCard key={event.id} event={event} onCardClick={onCardClick} />
