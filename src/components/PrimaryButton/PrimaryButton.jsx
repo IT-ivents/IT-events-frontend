e95c9249d2a6831a4ed2a380fdadd3d8bc1448e3
greybirbroman <98167513+greybirbroman@users.noreply.github.com>
2023-06-07 13:10:10 +0300

    fix: after review

--- src/components/PrimaryButton/PrimaryButton.jsx
+++ src/components/PrimaryButton/PrimaryButton.jsx
@@ -1,9 +1,13 @@
 import styles from './PrimaryButton.module.css';
 import RightArrow from './../../images/Arrows/arrow-right.svg';
 
-const PrimaryButton = ({ title, type }) => {
+const PrimaryButton = ({ title, type, onClick }) => {
   return (
-    <button className={styles.primaryButton} type={type || 'button'}>
+    <button
+      className={styles.primaryButton}
+      type={type || 'button'}
+      onClick={onClick}
+    >
       {title}
       <img src={RightArrow} alt="Arrow" />
     </button>
--- src/components/Subscribe/Subscribe.jsx
+++ src/components/Subscribe/Subscribe.jsx
@@ -4,13 +4,20 @@ import PrimaryButton from '../PrimaryButton/PrimaryButton';
 
 const Subscribe = () => {
   const [value, setValue] = useState('');
+  const [hidden, setHidden] = useState(false);
 
   const handleChange = (e) => {
     setValue(e.target.value);
   };
 
+  const handleHideClick = (e) => {
+    setHidden(true);
+  };
+
   return (
-    <section className={styles.subscribeSection}>
+    <section
+      className={`${styles.subscribeSection} ${hidden ? styles.hidden : ''}`}
+    >
       <h3 className={styles.headText}>Не пропустите лучшие события недели!</h3>
       <p className={styles.subtext}>
         Один раз в неделю мы будем высылать вам на почту подборку самых
@@ -25,9 +32,13 @@ const Subscribe = () => {
           value={value}
           onChange={handleChange}
         />
-        <PrimaryButton type="submit" title="Подписаться" />
+        <PrimaryButton type="submit" title="Подписаться" onClick={() => {}} />
       </form>
-      <button className={styles.alreadyButton} type="button">
+      <button
+        className={styles.alreadyButton}
+        type="button"
+        onClick={handleHideClick}
+      >
         Я уже подписался
       </button>
     </section>
