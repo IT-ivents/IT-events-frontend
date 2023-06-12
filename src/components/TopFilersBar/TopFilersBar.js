a841b9750b9751916546d0d72e9867234e65c9c5
Filatov Denis <demalaron@yandex.ru>
2023-06-12 10:48:23 +0300

    feat: add functionality top filter

--- src/components/TopFilersBar/TopFilersBar.js
+++ src/components/TopFilersBar/TopFilersBar.js
@@ -6,15 +6,35 @@ import Cross from './../../images/Actions/Close_gray.svg';
 const TopFilersBar = () => {
   const { values, setValues } = useContext(SearchFilterContext);
   const arr = Object.entries(values);
+
   const deleteValue = (item) => {
-    console.log(item);
     if (item === 'status' || item === 'tags' || item === 'specialities') {
       setValues({ ...values, [item]: [] });
     } else setValues({ ...values, [item]: null });
   };
 
+  const handleClearFilter = () => {
+    setValues({
+      status: [],
+      city: null,
+      date: null,
+      specialities: [],
+      price: null,
+      findTags: null,
+      tags: [],
+    });
+  };
+
+  const activeFilter = arr.filter((item) => item[1] && item[1].length !== 0);
+  const filterCount = activeFilter.length !== 0 ? activeFilter : null;
+
   return (
     <div className={styles.container}>
+      {filterCount && (
+        <div className={styles.countContainer}>
+          Фильры: {filterCount.length}
+        </div>
+      )}
       {arr.map((item, index) => {
         const text = () => {
           const name = item[0];
@@ -42,6 +62,11 @@ const TopFilersBar = () => {
           )
         );
       })}
+      {filterCount && (
+        <div onClick={handleClearFilter} className={styles.clearData}>
+          очистить все
+        </div>
+      )}
     </div>
   );
 };
