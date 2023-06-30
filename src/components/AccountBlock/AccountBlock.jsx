b7f5ecb1a4ecdcb923a91abda5903c37e30b633f
PolinaShchepochkina <polina007@inbox.ru>
2023-06-30 19:54:25 +0300

    feat: return back account page

--- src/components/AccountBlock/AccountBlock.jsx
+++ src/components/AccountBlock/AccountBlock.jsx
@@ -1,15 +1,13 @@
-// import styles from './AccountBlock.module.css';
+import styles from './AccountBlock.module.css';
 
-// const AccountBlock = ({ accoutBlockIcon, title, details }) => {
-//   return (
-//     <div className={styles.blockBorder}>
-//       <img src={accoutBlockIcon} alt="Icon" />
-//       <div className={styles.blockTitle}>{title}</div>
-//       <div className={styles.blockDetails}>{details}</div>
-//     </div>
-//   );
-// };
+const AccountBlock = ({ accoutBlockIcon, title, details }) => {
+  return (
+    <div className={styles.blockBorder}>
+      <img src={accoutBlockIcon} alt="Icon" />
+      <div className={styles.blockTitle}>{title}</div>
+      <div className={styles.blockDetails}>{details}</div>
+    </div>
+  );
+};
 
-// export default AccountBlock;
-
-// КОД ПОКА НЕ ИСПОЛЬЗУЕТСЯ, Т.К. НЕТ ЛК
+export default AccountBlock;
