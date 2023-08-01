80ef68810106368fb5c78db08940bd7bc82f6738
greybirbroman <98167513+greybirbroman@users.noreply.github.com>
2023-08-01 19:04:17 +0300

    feat: testing eventLoader and new router

--- src/index.js
+++ src/index.js
@@ -1,20 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Routes } from 'react-router-dom';
-
 import './index.css';
 import App from './components/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  // <AuthProvider>
-  //   <EventsProvider>
-  //     <FiltersProvider>
-  //       <ModalProvider>
-  //         <App />
-  //       </ModalProvider>
-  //     </FiltersProvider>
-  //   </EventsProvider>
-  // </AuthProvider>
-  <App />
-);
+root.render(<App />);
